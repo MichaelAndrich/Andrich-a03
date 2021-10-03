4841a051e09ecfe0db1d38ff929f99f25135cdde
Michael Andrich <michaelandrich@knights.ucf.edu>
2021-10-03 18:38:43 -0400

    1st commit

---
+++ exercise35/src/main/java/baseline/solution35.java
@@ -0,0 +1,38 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution 35
+ *  Copyright 2021 Michael Andrich
+ */
+
+package baseline;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class solution35 {
+    public static void main(String[] args ){
+        Scanner sc = new Scanner(System.in);
+
+        // like last exercise, use an array to store inputted names
+        ArrayList<String> ...
+        String input = null;
+
+        // prompt user for names but in a loop
+        do{
+            System.out.println("Enter a name: ");
+            input = sc.nextLine();
+
+            // create if statement that will make sure no blanks are stored in array
+            if (!input.isBlank() && !input.isEmpty());
+        }
+        // end do-while with a while statement
+        while(...)
+
+            // create a random generator for the winner
+
+            Random random = new Random();
+
+        // print out winner
+        System.out.println("The winner is...");
+
+    }
+}

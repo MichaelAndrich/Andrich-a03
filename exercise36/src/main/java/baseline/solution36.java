b2c42488244efbed90b9bf072846c4958e70aa83
Michael Andrich <michaelandrich@knights.ucf.edu>
2021-10-03 19:57:52 -0400

    1st commit

---
+++ exercise36/src/main/java/baseline/solution36.java
@@ -0,0 +1,44 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution 36
+ *  Copyright 2021 Michael Andrich
+ */
+
+
+package baseline;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class solution36 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // similar to last exercise, we must store our inputted numbers into an arraylist
+        ArrayList<> ... = new ArrayList<...>();
+
+        do{
+            // similar to last exercise, create another loop that takes n amount of numbers
+            // make sure to not read in the word done
+            if(!...) {
+
+            } catch(){
+
+            }
+        }
+    } while();
+}
+
+public static double average(){
+        // call array to get numbers and calc average
+        }
+        public static double min(){
+        // call array to get numbers and calc min
+        }
+public static double max(){
+        // call array to get numbers and calc max
+        }
+        public static double std(){
+        // call array to get numbers and calc standard deviation
+        }
+}
+

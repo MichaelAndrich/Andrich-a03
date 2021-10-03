cfaf5896e33952c9a0b3a132591d35e9053b365f
Michael Andrich <michaelandrich@knights.ucf.edu>
2021-10-02 22:12:50 -0400

    1st commit

---
+++ exercise30/src/main/java/baseline/solution30.java
@@ -0,0 +1,17 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution 30
+ *  Copyright 2021 Michael Andrich
+ */
+
+
+package baseline;
+
+public class solution30 {
+    //use for loops that create the table vertically
+    for(z...){
+        // nested for loop for horizontal
+        for(h...){
+            //print out v + h
+        }
+    }
+}
---
+++ exercise30/src/test/java/baseline/solution30test.java
@@ -0,0 +1,6 @@
+package baseline;
+
+
+//I don't even know how to setup a test case for this
+public class solution30test {
+}

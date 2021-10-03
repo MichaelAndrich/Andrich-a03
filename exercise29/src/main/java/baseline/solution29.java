3be882410419e4668759ed79ab4cc9f3e313f8d5
Michael Andrich <michaelandrich@knights.ucf.edu>
2021-10-02 21:37:45 -0400

    1st commit

---
+++ exercise29/src/main/java/baseline/solution29.java
@@ -0,0 +1,23 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution 29
+ *  Copyright 2021 Michael Andrich
+ */
+
+package baseline;
+
+public class solution29 {
+    // formula of a quick calculator for ROI
+    years = 72 / input;
+
+    // prompt user for input of 3 questions
+    System.out.prtintln("What is the rate of return? ");
+    // dont allow for the input of 0s
+    if(input >=1 && input <= 72){
+        System.out.println("It will take "+years+ " years to double your investment. ");
+        //print out amount of years to double investment
+    } else {
+
+    }
+        //print out if 0 is inputted to re-enter new value
+    System.out.println("0 is not a valid input. ")
+}
---
+++ exercise29/src/test/java/baseline/solution29test.java
@@ -0,0 +1,11 @@
+package baseline;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class solution29test {
+    @Test
+    void (){Assertions.assertEquals("", "");}
+
+
+}

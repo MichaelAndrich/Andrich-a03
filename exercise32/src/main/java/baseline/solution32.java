9a2ae046b3dcb37f68761c37a9c6cf97f93d7b1a
Michael Andrich <michaelandrich@knights.ucf.edu>
2021-10-03 17:01:52 -0400

    1st commit

---
+++ exercise32/src/main/java/baseline/solution32.java
@@ -0,0 +1,38 @@
+package baseline;
+
+public class solution32 {
+    // create a function to create the level of difficulty
+    // 1st diff is 1-10
+    try
+    if level == 1;
+        randNum = random.randint(1,10);
+    // let user know too high or too low
+    // 2nd diff is 1-100
+    if level == 2;
+        randNum = random.randint(1,100);
+    // let user know too high or too low
+    // 3rd diff is 1-1000
+    if level == 3;
+        randNum = random.randint(1,1000);
+        catch
+    // let user know too high or too low
+    // count non-numeric values as wrong guesses
+    // prompt user for difficulty level of game
+    System.out.println("Let's play Guess the Number!")
+
+    System.out.println("Enter the difficulty level (1, 2, or 3): ");
+    // allow numbers only for entry
+    if (guess =)
+    if (guess == randNum);
+        count = count+1;
+        break
+    else if (guess < randNum);
+        System.out.prtintln("Too low, guess again: ");
+        count = count+1;
+    else if (guess > randNum);
+        System.out.println("Too high, guess again: ");
+        count =  count+1
+    // once correctly guessed, print in how many guesses it took
+    good job youre so lucky
+    // prompt user Y/N to play again
+}

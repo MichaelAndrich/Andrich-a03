73e313b784923cbc0665cedfc3f60f53a67b2161
Michael Andrich <michaelandrich@knights.ucf.edu>
2021-10-02 20:50:09 -0400

    1st commit

---
+++ exercise28/src/main/java/baseline/solution28.java
@@ -0,0 +1,21 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution 28
+ *  Copyright 2021 Michael Andrich
+ */
+
+
+package baseline;
+
+import java.util.Scanner;
+
+public class solution28 {
+    public static void main (String[] args){
+        // use a counted loop to prompt user for 5 numbers
+        while(count < 6) {
+            System.out.println("Enter a number:");
+        }
+        // compute the total of the numbers
+        sum += inputted numbers
+            // print out total
+    }
+}
---
+++ exercise28/src/test/java/baseline/solution28test.java
@@ -0,0 +1,10 @@
+package baseline;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class solution28test {
+    @Test
+    void main() {Assertions.assertEquals("", "");}
+
+}

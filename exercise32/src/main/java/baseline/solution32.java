f5db7042d3132fb8166fdf533421475bc36fb273
Michael Andrich <michaelandrich@knights.ucf.edu>
2021-10-03 17:20:06 -0400

    2nd commit not final

--- exercise32/src/main/java/baseline/solution32.java
+++ exercise32/src/main/java/baseline/solution32.java
@@ -1,38 +1,60 @@
 package baseline;
 
-public class solution32 {
-    // create a function to create the level of difficulty
-    // 1st diff is 1-10
-    try
-    if level == 1;
-        randNum = random.randint(1,10);
-    // let user know too high or too low
-    // 2nd diff is 1-100
-    if level == 2;
-        randNum = random.randint(1,100);
-    // let user know too high or too low
-    // 3rd diff is 1-1000
-    if level == 3;
-        randNum = random.randint(1,1000);
-        catch
-    // let user know too high or too low
-    // count non-numeric values as wrong guesses
-    // prompt user for difficulty level of game
-    System.out.println("Let's play Guess the Number!")
+import java.util.Scanner;
 
-    System.out.println("Enter the difficulty level (1, 2, or 3): ");
-    // allow numbers only for entry
-    if (guess =)
-    if (guess == randNum);
-        count = count+1;
+public class solution32 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // create a function to create the level of difficulty
+        // 1st diff is 1-10
+        System.out.println("Enter the difficulty level (1, 2, or 3): ");
+        int level;
+        int randNum;
+        randNum = (int)(Math.random()* 999 +1);
+
+        try{
+            if(level == 1);{
+                randNum = (int)(Math.random()* 10 +1);
+            }
+            // let user know too high or too low
+            // 2nd diff is 1-100
+            else if(level == 2);{
+                randNum = (int)(Math.random()* 100 +1);
+            }
+            // let user know too high or too low
+            // 3rd diff is 1-1000
+            else(level == 3);{
+                randNum = (int)(Math.random()* 1000 +1);
+            }
+        }
+        catch (Exception e){
+            System.out.println("Please type 1, 2 or 3");
+        }
+        // let user know too high or too low
+        // count non-numeric values as wrong guesses
+        // prompt user for difficulty level of game
+        System.out.println("Let's play Guess the Number!")
+
+        System.out.println("Enter the difficulty level (1, 2, or 3): ");
+        level = sc.nextInt();
+
+
+        // allow numbers only for entry
+        int guess = 0;
+        int count;
+        if (guess =)
+            if (guess == randNum) ;
+        count = count + 1;
         break
-    else if (guess < randNum);
+    else if (guess < randNum) ;
         System.out.prtintln("Too low, guess again: ");
-        count = count+1;
-    else if (guess > randNum);
+        count = count + 1;
+    else if (guess > randNum) ;
         System.out.println("Too high, guess again: ");
-        count =  count+1
-    // once correctly guessed, print in how many guesses it took
-    good job youre so lucky
-    // prompt user Y/N to play again
+        count = count + 1
+        // once correctly guessed, print in how many guesses it took
+        good job youre so lucky
+        // prompt user Y/N to play again
+    }
 }
+

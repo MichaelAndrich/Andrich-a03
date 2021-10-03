ee67dc42bb17d75f54d610608e2293a092f0c87f
Michael Andrich <michaelandrich@knights.ucf.edu>
2021-10-03 17:30:42 -0400

    1st commit

---
+++ exercise33/src/main/java/baseline/solution33.java
@@ -0,0 +1,36 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution 33
+ *  Copyright 2021 Michael Andrich
+ */
+
+
+
+package baseline;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class solution33 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        // prompt user for question
+        System.out.println("What is your question? ");
+
+        // store in possible answers such as yes, no, maybe and ask again later
+        String [] response = (...)
+        // randomly choose one of those responses
+        String response = randomize...
+
+        // prtint out the magic answer
+        System.out.println(+response+);
+
+
+
+
+
+
+
+
+    }
+}

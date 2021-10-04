acfd1f49718f350d0828cec2d4c7a43909a1bca6
Michael Andrich <michaelandrich@knights.ucf.edu>
2021-10-03 22:46:47 -0400

    2nd commit final (testcase)

--- exercise33/src/test/java/baseline/solution33test.java
+++ exercise33/src/test/java/baseline/solution33test.java
@@ -1,9 +1,13 @@
 package baseline;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 //not sure how to use junit so :(
 public class solution33test {
     @Test
-    void main(){};
+    void main(){
+        Assertions.assertEquals("How do unit tests work?","How do unit tests work?");
+
+    }
 }

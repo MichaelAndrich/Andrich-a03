72bb878dacedb6e26dec79d6c27b814446f2ce65
Michael Andrich <michaelandrich@knights.ucf.edu>
2021-10-03 22:53:05 -0400

    3rd commit with testcase

---
+++ exercise34/src/test/java/baseline/solution34test.java
@@ -0,0 +1,21 @@
+package baseline;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class solution34test {
+    @Test
+    void main(){
+        Assertions.assertEquals("John Smith Jackie Jackson Chris Jones Amanda Cullen Jeremy Goodwin",
+                "John Smith Jackie Jackson Chris Jones Amanda Cullen Jeremy Goodwin");
+
+    }
+    @Test
+    void printEmployees(){
+        Assertions.assertEquals("Jackie Jackson Chris Jones Amanda Cullen Jeremy Goodwin",
+                "Jackie Jackson Chris Jones Amanda Cullen Jeremy Goodwin");
+
+    }
+}
+

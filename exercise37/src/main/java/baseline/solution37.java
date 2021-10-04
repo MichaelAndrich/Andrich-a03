c65db8fc025888c718785480bd45688307a06cf4
Michael Andrich <michaelandrich@knights.ucf.edu>
2021-10-03 20:59:48 -0400

    1st commit

---
+++ exercise37/src/main/java/baseline/solution37.java
@@ -0,0 +1,54 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution 37
+ *  Copyright 2021 Michael Andrich
+ */
+
+
+
+package baseline;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class solution37 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // prompt user for the minimum length of password
+        System.out.println("What's the minimum length? ");
+        // prompt user for how many special characters
+        System.out.println("How many special characters? ");
+        // prompt user for how many numbers
+        System.out.println("How many numbers? ");
+        // print out randomly generated password
+        System.out.println("Your password is "+randomPassword);
+
+
+        public static String randomPassword(int length, int spec, int nums){
+
+
+        // create a list for the alphabet a-z and A-Z
+        List<String> alphabet = new ArrayList<String>(...);
+
+        // create a list for special characters
+        List<String> specCharacters = new ArrayList<String>(...);
+
+        // create a list for numbers
+        List<Integer> numbers = new ArrayList<Integer>()...);
+
+
+
+        // create a loop to generate a random set of determined special characters
+        for(int = i;...)
+        // create a loop to generate a random set of determined numbers
+            for(int = i;...)
+        // create a loop to generate a random set of letters to fill the rest with the password
+                for(int = i;...)
+        // create a loop to generate a random set of all obtained special characters, numbers and letters
+                    for()
+
+
+        }
+    }
+}
